492eea3d59649e34c3a9c6bac7628c47ce0a064b
Thomas Dahlberg <thomas.dahlberg8@gmail.com>
2021-01-18 11:27:12 -0500

    Complete Input Component render tests

--- src/input.js
+++ src/input.js
@@ -3,18 +3,33 @@ import { connect } from 'react-redux';
 
 class Input extends Component {
   render() {
+    const contents = this.props.success
+      ? null
+      : (
+        <form className="form-inline">
+          <input
+            data-test="input-box"
+            className="mb-2 mx-sm-3"
+            type="text"
+            placeholder="enter guess" />
+          <button
+            data-test="submit-button"
+            className="btn btn-primary mb-2"
+            type="submit">
+              Submit
+          </button>
+        </form>
+      )
     return (
-      <div>
-        <button>
-
-        </button>
-        </div>
+      <div data-test="component-input">
+        {contents}
+      </div>
     )
   }
 };
 
-const mapStateToProps = (state) => {
-  return {};
+const mapStateToProps = ({success}) => {
+  return {success};
 }
 
 export default connect(mapStateToProps)(Input);
