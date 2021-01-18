5911badcd82b22ee672b8d5d0765a6fa98d74dad
Thomas Dahlberg <thomas.dahlberg8@gmail.com>
2021-01-18 10:49:59 -0500

    Set up Input component and storeFactory testUtils

---
+++ src/input.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+
+class Input extends Component {
+  render() {
+    return (
+      <div>
+        <button>
+
+        </button>
+        </div>
+    )
+  }
+};
+
+const mapStateToProps = (state) => {
+  return {};
+}
+
+export default connect(mapStateToProps)(Input);
