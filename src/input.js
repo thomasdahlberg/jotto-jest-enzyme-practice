1887b9b31832002e3d8801a107a1275f79a4a39a
Thomas Dahlberg <thomas.dahlberg8@gmail.com>
2021-01-22 21:26:28 -0500

    adds guessWord test

--- src/input.js
+++ src/input.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import { guessWord } from './actions';
 
-class Input extends Component {
+export class UnconnectedInput extends Component {
   render() {
     const contents = this.props.success
       ? null
@@ -17,6 +17,7 @@ class Input extends Component {
           <button
             data-test="submit-button"
             className="btn btn-primary mb-2"
+            onClick={() => this.props.guessWord('train')}
             type="submit">
               Submit
           </button>
@@ -34,4 +35,4 @@ const mapStateToProps = ({success}) => {
   return {success};
 }
 
-export default connect(mapStateToProps, { guessWord })(Input);
+export default connect(mapStateToProps, { guessWord })(UnconnectedInput);
--- src/input.test.js
+++ src/input.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import { findByTestAttr, storeFactory } from '../test/testUtils';
-import Input from './input';
+import Input, { UnconnectedInput } from './input';
 
 const setup = (initialState={}) => {
   const store = storeFactory(initialState)
@@ -62,5 +62,18 @@ describe('redux props', () => {
     const wrapper = setup();
     const guessWordProp = wrapper.instance().props.guessWord;
     expect(guessWordProp).toBeInstanceOf(Function);
+  });
+});
+describe('`guessWord` action creator call', () => {
+  test('`guessWord` runs on submit click', () => {
+    const guessWordMock = jest.fn();
+    const props = {
+      guessWord: guessWordMock
+    };
+    const wrapper = shallow(<UnconnectedInput {...props} />);
+    const submitButton = findByTestAttr(wrapper, 'submit-button');
+    submitButton.simulate('click');
+    const guessWordCallCount = guessWordMock.mock.calls.length;
+    expect(guessWordCallCount).toBe(1);
   })
-})
+});
