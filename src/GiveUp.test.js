5403a5d7787b6b77f9e21fecd48eac0fd7d37f7b
Thomas Dahlberg <thomas.dahlberg8@gmail.com>
2021-02-03 20:36:14 -0500

    stubs out redux challenge 3 tests and GiveUp component

---
+++ src/GiveUp.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import GiveUp from './GiveUp';
+
+const setup = () => {
+  return shallow(<GiveUp />);
+}
+
+describe('renders', () => {
+  test('renders without error', () => {
+
+  });
+  test('renders `Give Up` button', () => {
+
+  });
+
+  describe('when `Give Up button clicked', () => {
+    test('renders secret word text', () => {
+
+    });
+    test('renders message text', () => {
+
+    });
+    test('renders `New Word` component', () => {
+      
+    })
+  })
+});
