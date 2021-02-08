7857c07dd15cb3ce466f50d7ad55331fc26675b4
Thomas Dahlberg <thomas.dahlberg8@gmail.com>
2021-02-07 19:56:19 -0500

    Sets up GiveUp.test.js

--- src/GiveUp.test.js
+++ src/GiveUp.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { findByTestAttr } from '../test/testUtils';
 import { shallow } from 'enzyme';
 
 import GiveUp from './GiveUp';
@@ -9,15 +10,24 @@ const setup = () => {
 
 describe('renders', () => {
   test('renders without error', () => {
-
+    let wrapper = setup();
+    const component = findByTestAttr(wrapper, 'component-give-up');
+    expect(component.length).toBe(1);
   });
   test('renders `Give Up` button', () => {
-
+    let wrapper = setup();
+    const button = findByTestAttr(wrapper, 'give-up-button');
+    expect(button.length).toBe(1);
   });
 
   describe('when `Give Up button clicked', () => {
+    let wrapper = setup()
+    let button = findByTestAttr(wrapper, 'give-up-button');
+    beforeEach(() => {
+      button.simulate('click', { preventDefault() {} });
+    })
     test('renders secret word text', () => {
-
+      
     });
     test('renders message text', () => {
 
